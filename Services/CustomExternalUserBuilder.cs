d2ae6d3ee8b4646f63f355bdee2244ef96d978c0
Jose Dominguez <jose.dbaez@gmail.com>
2018-03-16 16:15:04 +0000

    add custom external builder example

---
+++ Services/CustomExternalUserBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Owin.Authentication.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using Sitecore.Diagnostics;
+
+namespace Sitecore9SSO.Services
+{
+    public class CustomExternalUserBuilder : Sitecore.Owin.Authentication.Services.DefaultExternalUserBuilder
+    {
+        public CustomExternalUserBuilder(bool isPersistentUser)
+            : base(isPersistentUser)
+        {
+        }
+
+        public CustomExternalUserBuilder(string isPersistentUser) 
+            : base(bool.Parse(isPersistentUser))
+        {
+        }
+
+        protected override string CreateUniqueUserName(UserManager<ApplicationUser> userManager, ExternalLoginInfo externalLoginInfo)
+        {
+            Assert.ArgumentNotNull(userManager, "userManager");
+            Assert.ArgumentNotNull(externalLoginInfo, "externalLoginInfo");
+
+            return externalLoginInfo.ExternalIdentity.FindFirstValue("FullName");
+        }
+    }
+}
